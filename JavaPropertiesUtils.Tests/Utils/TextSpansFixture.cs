0cab623fe7c6874f20709b1af4a4941046c6c756
Andrew Moore <andrew.moore@octopus.com>
2020-07-26 21:19:02 +1000

    Add an extension method to join TextSpans.

---
+++ JavaPropertiesUtils.Tests/Utils/TextSpansFixture.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using JavaPropertiesUtils.Utils;
+using NUnit.Framework;
+using Superpower.Model;
+
+namespace JavaPropertiesUtils.Tests.Utils
+{
+    [TestFixture]
+    public class TextSpansFixture
+    {
+        public class Joins
+        {
+            [Test]
+            public void ReturnsTheExpectedInputIfThereIsOnlyOneInput()
+            {
+                var source = "0123456789";
+                var input = MakeSpan(source, 0, 5);
+                var actual = TextSpans.Join(input);
+
+                actual.Position.Absolute.Should().Be(0);
+                actual.Length.Should().Be(5);
+            }
+            
+            [Test]
+            public void ReturnsTheExpectedSpanWithTwoInputs()
+            {
+                var source = "0123456789";
+                var actual = TextSpans.Join(
+                    MakeSpan(source, 1, 3),
+                    MakeSpan(source, 4, 2)
+                );
+                actual.Position.Absolute.Should().Be(1);
+                actual.Length.Should().Be(5);
+            }
+
+            [Test]
+            public void ReturnsTheExpectedSpanWithMultipleInputsInNonSequentialOrder()
+            {
+                var source = "0123456789";
+                var actual = TextSpans.Join(
+                    MakeSpan(source, 2, 1),
+                    MakeSpan(source, 1, 3),
+                    MakeSpan(source, 3, 4)
+                );
+                actual.Position.Absolute.Should().Be(1);
+                actual.Length.Should().Be(6);
+            }
+
+            private TextSpan MakeSpan(string source, int skip, int take)
+            {
+                return new TextSpan(source, Position.Zero, 0)
+                    .Skip(skip)
+                    .First(take);
+            }
+        }
+    }
+}
---
+++ JavaPropertiesUtils/Utils/TextSpans.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Superpower.Model;
+
+namespace JavaPropertiesUtils.Utils
+{
+    public static class TextSpans
+    {
+        /// <summary>
+        /// Joins multiple spans together, producing a single span
+        /// that covers all of the input spans.
+        /// </summary>
+        public static TextSpan Join(params TextSpan[] spans)
+        {
+            if (spans == null) throw new ArgumentNullException(nameof(spans));
+            if (spans.Length == 1) return spans[0];
+            
+            var first = spans.OrderBy(s => s.Position.Absolute).First();
+            var last = spans.OrderByDescending(s => s.Position.Absolute + s.Length).First();
+            var length = last.Position.Absolute + last.Length - first.Position.Absolute;
+            
+            return new TextSpan(
+                first.Source,
+                first.Position,
+                length
+            );
+        }
+        
+        /// <summary>
+        /// Joins multiple spans together, producing a single span
+        /// that covers all of the input spans.
+        /// </summary>
+        public static TextSpan Join(IEnumerable<TextSpan> spans)
+        {
+            return Join(spans.ToArray());
+        }
+    }
+}
